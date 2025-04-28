7cff8a0b590d23354151e9410eed45b5fd523d41
Kongemal357 <kongemal357@gmail.com>
2025-04-28 15:29:50 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -17,10 +17,7 @@ async def lifespan(app: FastAPI):
     await close_engine()
 
 
-app = FastAPI(
-    lifespan=lifespan,
-    redirect_slashes=False
-)
+app = FastAPI(lifespan=lifespan, redirect_slashes=False)
 
 
 @app.get("/recipes", response_model=List[schemas.RecipesListOut])
